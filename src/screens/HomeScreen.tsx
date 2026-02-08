5ad540cb113369432f8cdd84631213d75e822e90
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-02-09 02:27:47 +0530

    feat: Add HowItWorks, IntegratedServices, LabTestCTA, and Specialities components and integrate them into HomeScreen.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
+import IntegratedServices from '../components/IntegratedServices';
+import BookAppointmentCTA from '../components/BookAppointmentCTA';
+import HealthPackages from '../components/HealthPackages';
+import LabTestCTA from '../components/LabTestCTA';
+import AbhaIdSection from '../components/AbhaIdSection';
+import HowItWorks from '../components/HowItWorks';
+import Specialities from '../components/Specialities';
 
 const HomeScreen = () => {
     return (
@@ -10,6 +17,15 @@ const HomeScreen = () => {
             <Navbar />
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                 <Hero />
+                <IntegratedServices />
+                <BookAppointmentCTA />
+                <Specialities />
+                <HealthPackages />
+                <LabTestCTA />
+                <HowItWorks />
+                <AbhaIdSection />
+                {/* Repeating Book Appointment CTA as per user request point 8 if desired, or leaving it as one strong CTA */}
+                <View className="h-4" />
             </ScrollView>
             <Footer />
         </SafeAreaView>
