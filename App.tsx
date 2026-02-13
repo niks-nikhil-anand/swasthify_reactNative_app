2e8fc72e4f34949169125d93d35b38e12bc49ea9
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-02-14 02:29:17 +0530

    feat: Rename app to Swastify, integrate react-native-vector-icons with custom fonts, and set up NavigationContainer with react-native-reanimated.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import "./global.css";
 
 function App(): React.JSX.Element {
     return (
-        <AppNavigator />
+        <NavigationContainer>
+            <AppNavigator />
+        </NavigationContainer>
     );
 }
 
--- ios/SwastifyApp/AppDelegate.swift
+++ ios/SwastifyApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "SwastifyApp",
+      withModuleName: "Swastify",
       in: window,
       launchOptions: launchOptions
     )
